8cb9628272d63d9618060b8e2e14f738970dc8b3
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-11 00:03:29 +0200

    Created Queries Reducer Test Suit ( ++ updated dummy queries factory

--- src/store/queryFactory.js
+++ src/store/queryFactory.js
@@ -1,9 +1,10 @@
+// Dummy Queries Factory, Generates Queries for testing
 export const queryFactory = (id = 1) => ({
   id,
-  name: `Query Name ${id}`,
-  description: `Small description for the SPARQL query. ${id}`,
-  creator: `Spar Kewl ${id}`,
-  query: `select * { ?s ?p ?o . } ${id}`
+  name: `QName-${id}`,
+  description: `description-${id}`,
+  creator: `Author-${id}`,
+  query: `queryString-${id}`
 });
 
 export const queriesFactory = queriesNumber => {
