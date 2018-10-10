7f847e838c14578a7b013376dcba209982b05ffb
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-10 18:22:04 +0200

    create a simple test script for app comp to test testing env

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App Test Suit", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it("renders h1 with 'Hello React'", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1")).toBeTruthy();
+  });
 });
