cc1011605439b5ce36621469b0b000c3267c1fc6
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-11 10:41:55 +0200

    wrapped the app with a react browser router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
+//import { connect } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  //Redirect
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import createStore from "./store/createStore";
+import HomePage from "./components/HomePage";
+import DashboardPage from "./components/DashboardPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello React!</h1>
-        </header>
-      </div>
-    );
-  }
-}
+/*import { dataCreateQuery, dataGetQueries } from "./store/actions/queries";
+import { queryFactory } from "./queryFactory";*/
+
+const store = createStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/dashboard" component={DashboardPage} />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
 export default App;
+
+/*const App = props => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Hello React!</h1>
+      <br />
+      <button
+        onClick={() => {
+          const q = queryFactory(222);
+          //props.createQuery(q);
+          props.getQueries();
+        }}
+      >
+        Click
+      </button>
+    </header>
+  </div>
+);
+
+const mapDispatchToProps = dispatch => ({
+  createQuery: query => dispatch(dataCreateQuery(query)),
+  getQueries: () => dispatch(dataGetQueries())
+});
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(App);*/
