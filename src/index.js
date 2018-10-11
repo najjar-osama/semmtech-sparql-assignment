9b0fcbd969c664e69201ebbb9e60e1429139796b
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-11 10:43:31 +0200

    set to unregister service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import "react-app-polyfill/ie9";
+import "whatwg-fetch"; // fetch pollyfill
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import App from "./App";
+
 import * as serviceWorker from "./serviceWorker";
+/*import * as api from "./api/api";
+import { queryFactory } from "./queryFactory";
+import { dataCreateQuery } from "./store/actions/queries";*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.register();
+serviceWorker.unregister();
