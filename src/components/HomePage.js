ba5d7d7e7b983d3472cca2a806eab393e5e324a0
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-11 10:40:51 +0200

    created Home & Dashboard Pages

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../assets/images/linked_cells.png";
+import "../assets/images/dots.png";
+const HomePage = props => (
+  <div className="home-page">
+    <div className="home-page__bg-wrapper">
+      <div className="home-page__content-wrapper flex container">
+        <h1 className="home-page__title">
+          SPARQL Buddy <small>v0.1</small>
+        </h1>
+        <br />
+        <h2 className="home-page__subtitle">Your SPARQL Queries Maker!</h2>
+        <br />
+        <div className="home-page__btn-wrapper">
+          <button
+            className="home-page__btn"
+            onClick={() => props.history.push("/dashboard")}
+          >
+            Let's Start!
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
