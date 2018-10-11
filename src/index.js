10a418e0724a79a876e0761907e2f4e3d34e41b9
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-11 16:22:50 +0200

    cleanup comments

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import App from "./App";
-
 import * as serviceWorker from "./serviceWorker";
-/*import * as api from "./api/api";
-import { queryFactory } from "./queryFactory";*/
-import { dataGetQueries } from "./store/actions/queries";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
-
-console.log(process.env.REACT_APP_API_KEY);
-dataGetQueries();
